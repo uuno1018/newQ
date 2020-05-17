ac285cca5e778682e1a48678f72d2f5b0d8f346d
uuno1018 <satoru.uno1018@gmail.com>
2020-05-17 19:33:59 +0900

    filterでチェック済の要素を無くしたいがよく分からないエラーが出てしまう

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,21 @@
   <div class="home">
     <h1>ToDoリスト</h1>
     <div class="nyuryoku">
-    <input v-model="tt" type="text" placeholder="やること入力">
+    <input class="yarukoto" v-model="tt" type="text" placeholder="やること入力">
     <button @click="toadd">追加</button>
     </div>
-    <div class="todo" v-for="todo in todos" :key="todo.key">
-      <p>{{todo.to}}</p>
+    <div class="p">
+    <p>終わったらチェックを入れてクリア</p>
+    </div>
+    <ul class="todo">
+    <li class="to" v-for="todo in todos" :key="todo.key">
+      <input type="checkbox" id="check" v-model="todo.check">
+      <label for="check">{{todo.todo}}</label>
+    </li>
+    </ul>
+    <div class="btn">
+      <button @click="clear(todos)" class="a11">clear!</button>
+      <button class="a11">過去にしたこと</button>
     </div>
   </div>
 </template>
@@ -16,31 +26,43 @@ export default {
   data(){
     return{
       todos: [],
-      tt:""
+      tt:"",
     };
   },
   methods:{
+    async get(){
+      var db = firebase.firestore();
+      db.collection("todos")
+        .get()
+        .then(a => {
+          a.forEach(doc => {
+            var data = doc.data();
+            this.todos.push(data);
+          })
+      })
+    },
     toadd(){
       var db = firebase.firestore();
       db.collection("todos")
         .add({
           todo:this.tt
         })
+        this.todos.push(this.tt);
+        alert("追加しました");
+        this.tt="";
     },
-    async get(){
-      var db = firebase.firestore();
-      db.collection("todos")
-        .get()
-        .then(query => {
-          query.forEach(doc => {
-          var data = doc.data();
-          this.todos.push(data);
-        })
-      })
-    },
+    clear(todos){
+      console.log(this.todos);
+      console.log("aaa");
+      const newtodo = todos.filter(function(){
+        return this.todo.check === false
+      }
+      )
+      console.log(newtodo);
+    }
+  },
   created(){
     this.get();
-  }
   }
 }
 </script>
@@ -56,28 +78,56 @@ export default {
   margin: 0 auto;
   margin-top: 30px;
 }
-input{
+.yarukoto{
   border: gray 1px solid;
+  box-sizing: border-box;
   border-radius: 10px;
   padding: 5px;
-  width: 80%;
+  width: 89%;
 }
 button{
-  width: 15%;
+  width: 10%;
   cursor: pointer;
   padding: 3px 5px;
   border: 1px gray solid;
+  box-sizing: border-box;
   border-radius: 5px;
   background-color: white;
   color: gray;
-  transition: 0.3s;
+  box-shadow: 0 4px;
 }
-button:hover{
-  color: white;
-  background-color: grey;
-  border: 1px white solid;
+button:active{
+  position: relative;
+  top: 4px;
+  box-shadow: none;
 }
-.todo{
-  color: black;
+.p{
+  margin-top: 20px;
+}
+label{
+  margin-left: 10px;
+}
+.to{
+  width: 80%;
+  margin: 20px auto;
+  height: 30px;
+  line-height: 30px;
+  list-style:none;
+  text-align: left;
+}
+.check{
+  height: 12.67px;
+  width: 12.67px;
+  line-height: 30px;
+  margin-right: 10px;
+}
+.btn{
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
+  margin: 20px auto;
+}
+.a11{
+  width: 47%;
 }
 </style>
